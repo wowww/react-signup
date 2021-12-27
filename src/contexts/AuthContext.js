d7d295b14bb3c85e04c460cb50e738b0c8ef1f65
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-27 10:46:11 +0900

    feat. 사용자 등록 함수

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { auth } from '../utils/init-firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 const AuthContext = createContext({
   currentUser: null,
@@ -9,6 +10,10 @@ export const useAuth = () => useContext(AuthContext)
 
 export default function AuthContextProvider({children}) {
   const [ currentUser, setCurrentUser ] = useState(null)
+
+  function register(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
   
   const value = {
     currentUser,
