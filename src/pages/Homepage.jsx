1213ba191566bbe3e93250cf80c7ea901cd434c3
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-27 09:28:06 +0900

    docs. useAuth로 값 가져와서 currentUser 추가

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -12,12 +12,15 @@ import {
 import React from 'react'
 import { Layout } from '../components/Layout'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Homepage() {
+  const { currentUser } = useAuth()
+
   return (
     <Layout>
       <Heading>Home page</Heading>
-      <Text my={6}></Text>
+      <Text my={6}>{`The current user is: ${currentUser}`}</Text>
 
       <Heading>
         Firebase Authentication
