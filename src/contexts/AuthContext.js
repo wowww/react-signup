6f57191e0e12f6d0a193c2c8ee477d93b2b1a8f9
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-27 11:25:33 +0900

    feat. onAuthStateChanged

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,10 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { auth } from '../utils/init-firebase'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  onAuthStateChanged 
+} from 'firebase/auth'
 
 const AuthContext = createContext({
   currentUser: null,
@@ -12,6 +16,15 @@ export const useAuth = () => useContext(AuthContext)
 
 export default function AuthContextProvider({children}) {
   const [ currentUser, setCurrentUser ] = useState(null)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user)
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
 
   function register(email, password) {
     return createUserWithEmailAndPassword(auth, email, password)
