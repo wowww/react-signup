add4ecf6930ed53a02450dfb90acf766b0d5fccf
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-27 14:02:22 +0900

    feat. 로그아웃 상태일 때, 로그인 상태일 때 접근 불가능한 페에지에 대한 처리

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  useLocation,
+} from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import ForgotPasswordPage from '../pages/ForgotPasswordPage'
 import Homepage from '../pages/Homepage'
 import Loginpage from '../pages/Loginpage'
 import NotfoundPage from '../pages/NotfoundPage'
 import Profilepage from '../pages/Profilepage'
-import ProtectedPage from '../pages/ProtectedPage'
 import Registerpage from '../pages/Registerpage'
 import ResetPasswordPage from '../pages/ResetPasswordPage'
 
@@ -16,12 +21,19 @@ export default function AppRouter(props) {
       <Router>
         <Switch>
           <Route exact path='/' component={Homepage} />
-          <Route exact path='/login' component={Loginpage} />
-          <Route exact path='/register' component={Registerpage} />
+          <ProtectedRoute exact path='/login' component={Loginpage} />
+          <ProtectedRoute exact path='/register' component={Registerpage} />
           <ProtectedRoute exact path='/profile' component={Profilepage} />
-          <ProtectedRoute exact path='/protected-page' component={ProtectedPage} />
-          <Route exact path='/forgot-password' component={ForgotPasswordPage} />
-          <Route exact path='/reset-password' component={ResetPasswordPage} />
+          <ProtectedRoute
+            exact
+            path='/forgot-password'
+            component={ForgotPasswordPage}
+          />
+          <ProtectedRoute
+            exact
+            path='/reset-password'
+            component={ResetPasswordPage}
+          />
           <Route exact path='*' component={NotfoundPage} />
         </Switch>
       </Router>
@@ -32,9 +44,30 @@ export default function AppRouter(props) {
 function ProtectedRoute(props) {
   const { currentUser } = useAuth()
   const { path } = props
+  console.log('path', path)
+  const location = useLocation()
+  console.log('location state', location.state)
 
-  return currentUser ? <Route {...props} /> : <Redirect to={{
-    pathname: './login', 
-    state: {form: path}
-  }} />
+  if (
+    path === '/login' ||
+    path === '/register' ||
+    path === '/forgot-password' ||
+    path === '/reset-password'
+  ) {
+    return currentUser ? (
+      <Redirect to={location.state?.from ?? '/profile'} />
+    ) : (
+      <Route {...props} />
+    )
+  }
+  return currentUser ? (
+    <Route {...props} />
+  ) : (
+    <Redirect
+      to={{
+        pathname: '/login',
+        state: { from: path },
+      }}
+    />
+  )
 }
